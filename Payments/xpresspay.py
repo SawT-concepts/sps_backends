64f95701084defcbfae15bfbf0668d5c397851fd
sheryf Habib <80783021+sheryf0@users.noreply.github.com>
2023-01-19 09:29:14 +0100

    changed the base url from validate to verify

--- Payments/xpresspay.py
+++ Payments/xpresspay.py
@@ -3,17 +3,15 @@ from django.conf import settings
 #? Class for processing all Xpress requests from their api
 class XpressPay:
     XPRESS_PAY_PUBLIC_KEY = settings.XPRESS_PAY_PUBLIC_KEY
-    base_url = "https://myxpresspay.com:6004/api/Payments/ValidatePayment"
+    base_url = "https://myxpresspay.com:6004/api/Payments/VerifyPayment"
 
     def verify_payment (self, ref, *args, **kwargs):
 
         data = {
-            "publicKey": self.XPRESS_PAY_PUBLIC_KEY,
             "transactionId": ref,
-            "mod": "Debug",
         }
 
-        # replace here with xpress pay secret key
+        #todo replace here with xpress pay secret key
         headers = {
             "Authorization": f"Bearer {self.XPRESS_PAY_PUBLIC_KEY}",
             "Content-Type": "application/json"
